a14632ab16fa108b885fb26bc90c10b324cc909c
Chelsea Komlo <chelsea.komlo@gmail.com>
2013-05-20 16:55:21 -0600

    cleaned up rover class

--- lib/rover/rover.rb
+++ lib/rover/rover.rb
@@ -11,18 +11,12 @@ class Rover
     [x, y]
   end
 
-  #make the parser do this, and make it more abstract
-  #something that reads in the file, tells the parser to parse it, and takes the parsed instructions
-  #and sends to rover
-  #make a runner class that takes in file, sends to parser, and then sends instructions to rover.
-
   def evaluate(instructions)
     instructions.each do |c|
       case c
       when 'M' then move
       when 'L' then turn_left
       when 'R' then turn_right
-      else 
       end
     end
   end
@@ -34,11 +28,11 @@ class Rover
   end
 
   def move
-    if self.direction == 'N'
+    if direction == 'N'
       self.y += 1
-    elsif self.direction == 'E'
+    elsif direction == 'E'
       self.x += 1
-    elsif self.direction == 'W'
+    elsif direction == 'W'
       self.x -= 1
     else
       self.y -= 1
@@ -46,32 +40,14 @@ class Rover
   end
 
   def turn_left
-    index = compass_points.index(self.direction)
-    self.direction = compass_points[index - 1]
+    index = compass_points.index(direction)
+    direction = compass_points[index - 1]
   end
 
   def turn_right
-    index = compass_points.index(self.direction)
+    index = compass_points.index(direction)
     self.direction = compass_points[ (index + 1) % 4]
   end
 end 
 
-#allow the user to pass in instructions in a binary
-
-# init_string = gets()   # 3 1 N
-# instructions = gets()  # MLMLMLML
-
-# x, y, direction = init_string.split(" ")
-# rover = Rover.new(x.to_i, y.to_i, direction)
-# rover.evaluate(instructions)
-
-# puts "#{rover.x} #{rover.y} #{rover.direction}"
-  # Sets the x position of the rover. Restricts the position
-  # to within the grid.
-  # def x=(value)
-  #   if value >= 0 || value <= grid.width
-  #     @x = value
-  #   end
-  #   return @x
-  # end
 
