a8f23ef91bc183c50a5b8b473eaed4780e54a69c
Andrew Allen <achew22@achew22.com>
2013-06-09 15:27:11 -0600

    Implement moves as a list of movement vectors

--- lib/mars/rover.rb
+++ lib/mars/rover.rb
@@ -1,22 +1,27 @@
+require "matrix"
+
 class Rover
   attr_accessor :x, :y, :direction
 
   def initialize(x, y, direction)
-    @x = x
-    @y = y
+    place(x,y)
     @direction = direction
   end
 
+  def place(x,y)
+    @position_ = Vector[x, y]
+  end
+
   def position
-    [x, y]
+    @position_.to_a
   end
 
   def evaluate(instructions)
     instructions.each do |c|
       case c
-      when 'M' then move
-      when 'L' then turn_left
-      when 'R' then turn_right
+        when 'M' then move
+        when 'L' then turn_left
+        when 'R' then turn_right
       end
     end
   end
@@ -27,27 +32,29 @@ class Rover
     [ 'N', 'E', 'S', 'W' ]
   end
 
+  def move_matrix
+    [
+      Vector[ 0, 1], # North
+      Vector[ 1, 0], # East
+      Vector[ 0,-1], # South
+      Vector[-1, 0]  # West
+    ]
+  end
+
   def move
-    if direction == 'N'
-      self.y += 1
-    elsif direction == 'E'
-      self.x += 1
-    elsif direction == 'W'
-      self.x -= 1
-    else
-      self.y -= 1
-    end
+    index = compass_points.index(@direction)
+    @position_ = @position_ + move_matrix[index]
   end
 
   def turn_left
-    index = compass_points.index(direction)
-    self.direction = compass_points[index - 1]
+    index = compass_points.index(@direction)
+    @direction = compass_points[index - 1]
   end
 
   def turn_right
-    index = compass_points.index(direction)
-    self.direction = compass_points[ (index + 1) % 4]
+    index = compass_points.index(@direction)
+    @direction = compass_points[ (index + 1) % 4]
   end
-end 
+end
 
 
--- spec/rover_spec.rb
+++ spec/rover_spec.rb
@@ -1,43 +1,54 @@
 require 'spec_helper'
 
-describe Rover do 
+describe Rover do
 
-  describe "#initialize" do 
+  describe "#initialize" do
 
-    it "sets the rover's initial position" do 
+    it "sets the rover's initial position" do
       rover = Rover.new(0, 0, 'N')
       expect(rover.position).to eq [0, 0]
     end
   end
 
-  describe ".evaluate" do 
+  describe ".evaluate" do
 
     let(:rover) { Rover.new(0, 0, 'N') }
 
-    context "when valid instructions are given" do 
+    context "when valid instructions are given" do
 
-      it "returns the rover's final position without turning" do 
+      it "returns the rover's final position without turning" do
         rover.evaluate(['M', 'M'])
         expect(rover.position).to eq [0, 2]
       end
 
-      it "returns the rover's final position when turning right" do 
+      it "returns the rover's final position when turning right" do
         rover.evaluate(['R', 'M'])
         expect(rover.position).to eq [1, 0]
       end
 
-      it "returns the rover's final position when starting from a different starting point" do 
-        rover.x, rover.y = 1, 2
+      it "returns the rover's final position when starting from a different starting point" do
+        rover.place 1, 2
         rover.evaluate(['L', 'M', 'L', 'M', 'L', 'M', 'L', 'M', 'M'])
         expect(rover.position).to eq [1, 3]
         expect(rover.direction).to eq 'N'
       end
 
-      it "returns the rover's position when starting from another direction" do 
-        rover.x, rover.y, rover.direction = 3, 3, 'E'
+      it "returns the rover's position when starting from another direction" do
+        rover.place 3, 3
+        rover.direction = 'E'
         rover.evaluate(['M', 'M', 'R', 'M', 'M', 'R', 'M', 'R', 'R', 'M'])
         expect(rover.position).to eq [5, 1]
         expect(rover.direction).to eq 'E'
+      end
+
+      it "turns completely around clockwise" do
+        rover.evaluate(['R', 'R', 'R', 'R'])
+        expect(rover.direction).to eq 'N'
+      end
+
+      it "turns completely around counter-clockwise" do
+        rover.evaluate(['L', 'L', 'L', 'L'])
+        expect(rover.direction).to eq 'N'
       end
     end
   end
